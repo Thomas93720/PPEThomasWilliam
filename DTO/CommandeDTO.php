451f4bffcf9c8f6c1ed6195d440378a009c99a9c
fh <thomasprezot63@gmail.com>
2020-05-14 08:42:18 +0200

    jhoidfv

---
+++ DTO/CommandeDTO.php
@@ -0,0 +1,47 @@
+<?php
+class Commande
+{
+    private $idCommande ;
+    private $idTacos ;
+    private $idSupplement ;
+    private $prix ;
+    
+    function getIdCommande() 
+    {
+        return $this->idCommande;
+    }
+
+    function getIdTacos() 
+    {
+        return $this->idTacos;
+    }
+
+    function getIdSupplement() 
+    {
+        return $this->idSupplement;
+    }
+
+    function getPrix() 
+    {
+        return $this->prix;
+    }
+
+    function setIdCommande($idCommande) 
+    {
+        $this->idCommande = $idCommande;
+    }
+
+    function setIdTacos($idTacos) {
+        $this->idTacos = $idTacos;
+    }
+
+    function setIdSupplement($idSupplement) 
+    {
+        $this->idSupplement = $idSupplement;
+    }
+
+    function setPrix($prix) 
+    {
+        $this->prix = $prix;
+    }
+}
