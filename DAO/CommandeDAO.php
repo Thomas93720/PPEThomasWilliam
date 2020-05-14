84cca092fbb3e8c5ff8f54dc4a9bd6ed8415f3d3
fh <thomasprezot63@gmail.com>
2020-05-14 09:30:43 +0200

    CommandeDAO

--- DAO/CommandeDAO.php
+++ DAO/CommandeDAO.php
@@ -2,23 +2,31 @@
 class CommandeDAO
 {
     include_once("Tools/DataBaseLinker.php");
-	include_once("DTO/Tacos.php");
-    public static function findTacos($idTacos){
+	include_once("DTO/CommandeDTO.php");
+    public static function findCommande($idCommande)
+    {
         $initCo = dataBaseLinker::getConnexion();
-
-        $state = $initCo->prepare("SELECT * FROM Tacos WHERE idTacos =?");
-        $state->bindParam(1,$idTacos);
+        $state = $initCo->prepare("SELECT * FROM Commande WHERE idCommande =?");
+        $state->bindParam(1,$idCommande);
         $state->execute();
-
         $resultats = $state->fetchAll();
-        
         foreach ($resultats as $lineResultats){
-            $tacos = new Tacos();
-            $tacos->setIdTacos($lineResultats["idTacos"]);
-            $tacos->setTaille($lineResultats["taille"]);
-            $tacos->setPrix($lineResultats["prix"]);
+            $Commande = new Commande();
+            $Commande->setIdCommande($lineResultats["idCommande"]);
+            $Commande->setIdTacos($lineResultats["idTacos"]);
+            $Commande->setPrix($lineResultats["prix"]);
+            $Commande->setIdSupplement($lineResultats["idSupplement"]);
         }
-        return $tacos;
+        return $Commande;
+    }
+    public static function InsertCommande($Commande)
+    {
+        $initCo = dataBaseLinker::getConnexion();
+        $state = $initCo->prepare("INSERT INTO Commande(idSupplement,idTacos,prix) VALUES (?,?,?)");
+        $state->bindParam(1,$Commande->getIdSupplement());
+        $state->bindParam(2,$Commande->getIdTacos());
+        $state->bindParam(3,$Commande->getPrix());
+        $state->execute();
     }
 }
 ?>
