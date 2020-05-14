fc846dd054c5504001ced351b1262df4f6c28c5d
William <william.bonnemoy@gmail.com>
2020-05-14 11:51:49 +0200

    dao tacos

--- DAO/TacosDAO.php
+++ DAO/TacosDAO.php
@@ -48,6 +48,7 @@
                     $Tacos->setPrix($lineResultats["prix"]);                  
                 }
                 return $Tacos;
+                
             }
         
         
