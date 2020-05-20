aa7ebc025b55ecbd28f722c89e5b073c74e3334b
William <william.bonnemoy@gmail.com>
2020-05-20 16:03:15 +0200

    story

---
+++ page/histoire/histoire.php
@@ -0,0 +1,5 @@
+Salut, c'est Xzibit, j'ai passé 10 ans de ma vie a custom et amélioré les pires voitures de Californie afin de rendre heureux leurs propriétaire. 
+Après 10 ans de tuning et après avoir réalisé les projets les plus fous du monde, je pense avoir fait le tour du tuning de bagnole. 
+Je me suis donc lancé dans le tuning de tacos. Les débuts on été difficile la mécanique c'est pas comme la cuisine,
+mais après 1 an d'apprentissage auprès des meilleurs tuneur de tacos au monde,
+ je suis enfin prêt pour vous fournir les tacos les plus ambitieux et novateurs 
